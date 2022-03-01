c51d820d9800a6fc2324335086298606bb93ada4
Bill ELsey <whelsey3@live.com>
2022-03-01 06:33:01 -0500

    Add project files.

---
+++ CustomerApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Data;
+using CustomerLib;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace CustomerApp.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+                public MainViewModel(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository ?? 
+                                  throw new ArgumentNullException("customerRepository");
+            AddCommand = new RelayCommand(DoAdd);
+            RemoveCommand = new RelayCommand(DoRemove, () => SelectedCustomer != null);
+            SaveCommand = new RelayCommand(DoSave);
+            SearchCommand = new RelayCommand<string>(DoSearch);
+        }
+
+        // ========== Properties ====================>
+        private readonly ICustomerRepository _customerRepository;
+
+        public IEnumerable<Customer> Customers => _customerRepository.Customers;
+
+        private Customer _selectedCustomer;
+        public Customer SelectedCustomer
+        {
+            get => _selectedCustomer;
+            set
+            {
+                SetProperty(ref _selectedCustomer, value);
+                RemoveCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        // ========== Commands ====================>
+
+        public IRelayCommand AddCommand { get; }
+        public IRelayCommand RemoveCommand { get; }
+        public IRelayCommand SaveCommand { get; }
+        public IRelayCommand<string> SearchCommand { get; }
+
+        private void DoAdd()
+        {
+            var customer = new Customer();
+            _customerRepository.Add(customer);
+            SelectedCustomer = customer;
+            OnPropertyChanged("Customers");
+        }
+
+        private void DoRemove()
+        {
+            if (SelectedCustomer != null)
+            {
+                _customerRepository.Remove(SelectedCustomer);
+                SelectedCustomer = null;
+                OnPropertyChanged("Customers");
+            }
+        }
+
+        private void DoSave()
+        {
+            _customerRepository.Commit();
+        }
+
+        private void DoSearch(string textToSearch)
+        {
+            var coll = CollectionViewSource.GetDefaultView(Customers);
+            if (!string.IsNullOrWhiteSpace(textToSearch))
+                coll.Filter = c => ((Customer)c).Country.ToLower().Contains(textToSearch.ToLower());
+            else
+                coll.Filter = null;
+        }
+    }
+}
